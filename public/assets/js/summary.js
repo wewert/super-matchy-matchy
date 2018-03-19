1cbeae97668a54cc75197fdfdf63f762e731cdb1
sosika <hewjang@gmail.com>
2018-03-19 00:01:46 -0600

    finalpush0318

--- public/assets/js/summary.js
+++ public/assets/js/summary.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
   var playerScore = $("#score");
+  var playerName = $("#name");
 
   // take score from previous page
+
+  $(document).on("submit", "#name", handlePlayerSubmitButton);
+
+  function handlePlayerSubmitButton(event) {
+    event.preventDefault();
+
+    if (!playerName.val().trim().trim()) {
+      return;
+    }
+    // Calling the upsertPlayer function and passing in the value of the player_name and points
+    upsertPlayer({
+      player_name: playerName.val().trim(),
+      points: playerScore.val().trim()
+    });
+  }
+
+  function upsertPlayer(playerData) {
+    $.post("/api/players", playerData);
+      // .then(getAuthors);
+  }
+
 }
