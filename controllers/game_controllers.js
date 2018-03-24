3b5aa0e1ab6916dbffe61ff398dbe66bdb4a817d
Amy Christine <amychristine29@gmail.comgit config --global user.name Amy>
2018-03-23 23:02:31 -0600

    leaderboard 95 done

--- controllers/game_controllers.js
+++ controllers/game_controllers.js
@@ -11,12 +11,20 @@ router.get("/leaderboard", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/leaderboard.html"))
 });
 
-router.get("/players", function (req, res) {
-  db.player.findAll()
-    .then(function (data) {
-      // console.log(data);
-      res.json(data);
-    });
+//plan is to pass value game level from gameboard/summary/toleaderboard until then hard coded 'E'.  
+router.get("/players/:game_level", function (req, res) {
+  db.player.findAll({
+    where: {
+      game_level: 'E'
+    },
+    order: [
+      ['points', 'DESC']
+    ]
+
+  }).then(function (data) {
+    // console.log(data);
+    res.json(data);
+  });
 });
 
 module.exports = router;
--- models/player.js
+++ models/player.js
@@ -9,7 +9,9 @@ module.exports = function (sequelize, Datatypes) {
             allowNull: false,
             isUppercase: true,
             validate: {
-                len: [1]
+                len: [1],
+                is: ['E', 'M', 'H'],
+                isUppercase: true
             },
         },
         points: {
